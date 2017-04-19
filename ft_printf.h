6cf8850c1aac3cc3ebce516eb4601b8a8fda196d
Louis Young <youngalou@gmail.com>
2017-04-19 14:59:52 -0700

    Initial commit for ft_printf.

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyoung <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/18 13:44:15 by lyoung            #+#    #+#             */
+/*   Updated: 2017/04/19 13:39:29 by lyoung           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include "./libft/libft.h"
+
+int		ft_printf(const char *format, ...);
+
+#endif
