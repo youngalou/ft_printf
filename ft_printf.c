5a1ce03f1b17441b35b9e76f39ab1af2d75210de
Louis Young <youngalou@gmail.com>
2017-04-20 13:53:44 -0700

    Function now also prints format before first %

--- ft_printf.c
+++ ft_printf.c
@@ -6,61 +6,46 @@
 /*   By: lyoung <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/19 11:24:26 by lyoung            #+#    #+#             */
-/*   Updated: 2017/04/20 13:14:53 by lyoung           ###   ########.fr       */
+/*   Updated: 2017/04/20 13:51:58 by lyoung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-/*void	handle_args(char *str, char c)
+void	handle_args(const char *fmt, va_list ap, char *place)
 {
-	if (c == 's')
+	char	conv;
+	char	*str;
+	int		nb;
+
+	conv = *(place + 1);
+	if (place - fmt)
 	{
+		str = ft_strndup(fmt, place - fmt);
+		ft_putstr(str);
+	}
+	if (conv == 's')
+	{
+		str = va_arg(ap, char*);
 		ft_putstr(str);
 		return ;
 	}
-	if (c == 'd')
+	else if (conv == 'd')
 	{
-		ft_putnbr(str);
+		nb = va_arg(ap, int);
+		ft_putnbr(nb);
 		return ;
 	}
-}*/
+}
 
-int		ft_printf(const char *format, ...)
+int		ft_printf(const char *fmt, ...)
 {
 	va_list		ap;
 	char		*place;
-	char		*str;
-	int			nb;
-	char		c;
 
-	va_start(ap, format);
-	while (*format)
-	{
-		if ((place = ft_strchr(format, '%')))
-		{
-			if (*(place + 1) == 's')
-			{
-				str = va_arg(ap, char*);
-				ft_putstr(str);
-				return (0);
-			}
-			if (*(place + 1) == 'd')
-			{
-				nb = va_arg(ap, int);
-				ft_putnbr(nb);
-				return (0);
-			}
-			if (*(place + 1) == 'c')
-			{
-				c = va_arg(ap, char);
-				ft_putchar(c);
-				return (0);
-			}
-		}
-		if (ft_strlen(format) > 0)
-			ft_putstr(format);
-		va_end(ap);
-	}
+	va_start(ap, fmt);
+	if ((place = ft_strchr(fmt, '%')))
+		handle_args(fmt, ap, place);
+	va_end(ap);
 	return (0);
 }
