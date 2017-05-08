30f14ca9abcdf3c32b6e55da854a230045faa2d6
Louis Young <youngalou@gmail.com>
2017-05-08 15:50:11 -0700

    handle_mods.c -> modifiers.c

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-gcc -Wall -Werror -Wextra main.c ft_printf.c build.c conversions.c handle_mods.c libft/libft.a
+gcc -Wall -Werror -Wextra main.c ft_printf.c build.c conversions.c modifiers.c libft/libft.a
 ./a.out
